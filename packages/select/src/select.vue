440350154b7888e72236909d8d88ee4f68de36b2
nicolasxiao <nicolasxiao@tencent.com>
2019-06-06 17:51:20 +0800

    feat: 下拉框优化

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -6,7 +6,7 @@
     v-clickoutside="handleClose">
     <div
       class="el-select__tags"
-      v-if="multiple"
+      v-if="multiple && false"
       ref="tags"
       :style="{ 'max-width': inputWidth - 32 + 'px', width: '100%' }">
       <span v-if="collapseTags && selected.length">
@@ -103,7 +103,15 @@
     </el-input>
     <div class="tea-dropdown" ref="reference" :class="{'is-disabled': selectDisabled}">
       <div class="tea-dropdown__header" :class="{'tea-dropdown-btn': border}">
-        <div class="tea-dropdown__value">{{selectedLabel || placeholder}}</div>
+        <div class="tea-dropdown__value">
+          <template v-if="multiple && selected.length > 0">
+            <span v-for="(item, index) in selected" :key="index">{{item.currentLabel}}{{index+1 === selected.length?'':', '}}</span>
+          </template>
+          <template v-else>
+            {{selectedLabel || placeholder}}
+          </template>
+          
+        </div>
         <i type="arrowdown" class="tea-icon tea-icon-arrowdown"></i>
       </div>
     </div>
@@ -118,7 +126,7 @@
         <el-scrollbar
           tag="ul"
           wrap-class="el-select-dropdown__wrap"
-          view-class="tea-list tea-list--option"
+          :view-class="'tea-list tea-list--option' + (multiple ? ' tea-list--checkoption' : '')"
           ref="scrollbar"
           :class="{ 'is-empty': !allowCreate && query && filteredOptionsCount === 0 }"
           v-show="options.length > 0 && !loading">
