bdc1e73a717cfc84c5e1cf66630a2a371fd9841c
nicolasxiao <nicolasxiao@tencent.com>
2019-06-08 19:44:48 +0800

    fix: 修复构建脚本bug

--- build/config.js
+++ build/config.js
@@ -4,8 +4,11 @@ var nodeExternals = require('webpack-node-externals');
 var Components = require('../components.json');
 
 var utilsList = fs.readdirSync(path.resolve(__dirname, '../src/utils'));
+var utilsListMenu = fs.readdirSync(path.resolve(__dirname, '../src/utils/menu'));
+var utilsListPopup = fs.readdirSync(path.resolve(__dirname, '../src/utils/popup'));
 var mixinsList = fs.readdirSync(path.resolve(__dirname, '../src/mixins'));
 var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'));
+var directivesList = fs.readdirSync(path.resolve(__dirname, '../src/directives'));
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
@@ -17,6 +20,14 @@ utilsList.forEach(function(file) {
   file = path.basename(file, '.js');
   externals[`element-ui/src/utils/${file}`] = `@tencent/honey-ui/lib/utils/${file}`;
 });
+utilsListMenu.forEach(function(file) {
+  file = path.basename(file, '.js');
+  externals[`element-ui/src/utils/menu/${file}`] = `@tencent/honey-ui/lib/utils/menu/${file}`;
+});
+utilsListPopup.forEach(function(file) {
+  file = path.basename(file, '.js');
+  externals[`element-ui/src/utils/popup/${file}`] = `@tencent/honey-ui/lib/utils/popup/${file}`;
+});
 mixinsList.forEach(function(file) {
   file = path.basename(file, '.js');
   externals[`element-ui/src/mixins/${file}`] = `@tencent/honey-ui/lib/mixins/${file}`;
@@ -24,6 +35,11 @@ mixinsList.forEach(function(file) {
 transitionList.forEach(function(file) {
   file = path.basename(file, '.js');
   externals[`element-ui/src/transitions/${file}`] = `@tencent/honey-ui/lib/transitions/${file}`;
+});
+
+directivesList.forEach(function(file) {
+  file = path.basename(file, '.js');
+  externals[`element-ui/src/directives/${file}`] = `@tencent/honey-ui/lib/directives/${file}`;
 });
 
 externals = [Object.assign({
