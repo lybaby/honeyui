1b8ccc65fac0a12b3a34d350e8ad86e2a0b68a58
nicolasxiao <nicolasxiao@tencent.com>
2019-06-25 10:37:43 +0800

    chore: build 2.9.1-r13

--- src/index.js
+++ src/index.js
@@ -208,7 +208,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.9.1-r12',
+  version: '2.9.1-r13',
   locale: locale.use,
   i18n: locale.i18n,
   install,
