a87482a85db07fb679dd21a16f35681c2a500b7f
tiny <tinyyue@tencent.com>
2019-10-24 20:26:36 +0800

    feat:打包文件

--- src/index.js
+++ src/index.js
@@ -208,7 +208,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.9.1-r24',
+  version: '2.9.1-r25',
   locale: locale.use,
   i18n: locale.i18n,
   install,
