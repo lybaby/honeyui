5fce13d08294b076c76afc9605566e205c8e7015
tiny <tinyyue@tencent.com>
2021-09-03 15:56:55 +0800

    feat: 2.9.1-r40 发布,静态文件放cdn

--- src/index.js
+++ src/index.js
@@ -210,7 +210,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.9.1-r37',
+  version: '2.9.1-r40',
   locale: locale.use,
   i18n: locale.i18n,
   install,
