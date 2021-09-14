58b4738248ac71f5cb956f47feb064b23c990e07
tiny <tinyyue@tencent.com>
2021-09-14 14:46:07 +0800

    feat: v41版本发布

--- src/index.js
+++ src/index.js
@@ -210,7 +210,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.9.1-r40',
+  version: '2.9.1-r41',
   locale: locale.use,
   i18n: locale.i18n,
   install,
