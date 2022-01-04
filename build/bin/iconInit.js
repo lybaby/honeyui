d18cda33a2651308b9dc14e1061f22855c1bc301
kristli <kristli@tencent.com>
2022-01-04 17:47:00 +0800

    chore: 文档更新，图标修复

--- build/bin/iconInit.js
+++ build/bin/iconInit.js
@@ -3,17 +3,25 @@
 var postcss = require('postcss');
 var fs = require('fs');
 var path = require('path');
-var fontFile = fs.readFileSync(path.resolve(__dirname, '../../packages/theme-chalk/src/icon.scss'), 'utf8');
+// var fontFile = fs.readFileSync(path.resolve(__dirname, '../../packages/theme-chalk/src/icon.scss'), 'utf8');
+var fontFile = fs.readFileSync(path.resolve(__dirname, '../../packages/theme-chalk/src/_tea.scss'), 'utf8');
 var nodes = postcss.parse(fontFile).nodes;
 var classList = [];
 
 nodes.forEach((node) => {
   var selector = node.selector || '';
-  var reg = new RegExp(/\.el-icon-([^:]+):before/);
+  // var reg = new RegExp(/\.el-icon-([^:]+):before/);
+  var reg = new RegExp(/^\.tea-icon-([^{]+)/);
   var arr = selector.match(reg);
+  let flag = true;
 
-  if (arr && arr[1]) {
-    classList.push(arr[1]);
+  if (node.nodes) {
+    const labelArr = node.nodes.map((child) => child.prop);
+    flag = labelArr.indexOf('background-size') > -1;
+  }
+
+  if (arr && arr[1] && flag && arr[1].indexOf(':hover') === -1) {
+    classList.push(arr[1].replace('.size-', ' size-'));
   }
 });
 
