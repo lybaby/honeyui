825c2a8bc5c2a8957d237f19ea46f15417b8b139
kristli <kristli@tencent.com>
2022-03-03 15:38:25 +0800

    chore: 移除自身node-sass依赖，发布r64

--- src/index.js
+++ src/index.js
@@ -214,7 +214,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.9.1-r61',
+  version: '2.9.1-r64',
   locale: locale.use,
   i18n: locale.i18n,
   install,
